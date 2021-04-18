367605ac152c4f24c209347aada154c467fe70f6
Ajay <ajaymungur@hotmail.com>
2021-04-18 15:40:28 +0100

    Added functionality to displaying when a user is typing and timing out when they go idle

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Demo socket.io Server
+
+The aim of this project was to try creating an API using `Express` and to make this the server for a chat app using `socketIO`.  
+
+The chat app is deployed at: https://ajays-practice-chat-app.herokuapp.com/
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('express')();
 const http = require('http').Server(app)
 const port = process.env.PORT || 8090;
-const url = "https://ajays-practice-chat-app.herokuapp.com"
-// const url = "http://localhost:3000"
+// const url = "https://ajays-practice-chat-app.herokuapp.com"
+const url = "http://localhost:3000"
 const io = require('socket.io')(http, {
   cors: {
     origin: url,
@@ -13,6 +13,12 @@ const io = require('socket.io')(http, {
 });
 
 let users = {}
+
+const msgTimeOut = (func, time, id) => {
+  users[id].timer = setTimeout(() => {
+    func()
+  }, time)
+}
 
 const testRoute = require('./testApis.js')
 const homeRoute = require('./home.js')
@@ -25,17 +31,35 @@ io.on('connection', (socket) => {
   socket.emit('connectionSuccess', socket.id)
 
   socket.on('addUser', (user) => {
-    users[user.id] = user.name;
+    users[user.id] = { name: user.name, typing: false, timeOut: msgTimeOut };
     io.emit('joined', `${user.name} joined the chat`);
   })
 
-  socket.on('typing', (name) => {
-    socket.broadcast.emit('typing', `${name} is typing`)
+  socket.on('typing', (id, name) => {
+    if(users[id].typing === false){
+      socket.broadcast.emit('typing', { id: id, message:`${name} is typing` })
+      users[id].typing = true;
+      users[id].timeOut(() => {
+        socket.broadcast.emit('stoppedTyping', id)
+        users[id].typing = false;
+      }, 3000, id)
+    } else {
+      clearTimeout(users[id].timer);
+      users[id].timeOut(() => {
+        socket.broadcast.emit('stoppedTyping', id)
+        users[id].typing = false;
+      }, 3000, id);
+    }
   })
 
   socket.on('chat message', (message) => {
     //change to emit to everyone but the sender
     socket.broadcast.emit('chat message', message);
+    if(users[socket.id].typing === true){
+      clearTimeout(users[socket.id]);
+      socket.broadcast.emit('stoppedTyping', socket.id)
+      users[socket.id].typing = false;
+    }
   })
 
   socket.on('disconnect', () => {
